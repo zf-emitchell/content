bfdaf89d8053870238cbbbfa3e87a8513540a40e
Dean Arbel <darbel@paloaltonetworks.com>
2020-09-15 18:58:15 +0300

    Created PowerShellRemoting anew

---
+++ Packs/PowerShellRemoting/Integrations/PowerShellRemoting/PowerShellRemoting.ps1
@@ -0,0 +1,85 @@
+. $PSScriptRoot\CommonServerPowerShell.ps1
+$global:HOSTNAME = $demisto.Params().hostname
+$global:USERNAME = $demisto.Params().credentials.identifier
+$global:PASSWORD = $demisto.Params().credentials.password
+
+function CreateSession ()
+{
+    <#
+    .Description
+    Creates a session to target machine using hostname, username and password
+    #>
+    $user = 'winrm\administrator'
+    $password=ConvertTo-SecureString 'dD(;RJP?bRDI8qz=9mK2XK)ul;T*AT2Q' –asplaintext –force
+    $Creds = new-object -typename System.Management.Automation.PSCredential -argumentlist $user, $password
+    $Session = New-PSSession -ComputerName 172.31.38.7 -Authentication Negotiate -credential $Creds -ErrorAction Stop
+    return $Session
+}
+
+function InvokeCommand ($Command)
+{
+    <#
+    .Description
+    Runs invoke-command on existing session.
+    .Example
+    Get-Process powershell
+    #>
+    $Title = "Result for PowerShell Remote SSH Command: $Command `n"
+    $Session = CreateSession
+    $Result = Invoke-Command $Session -ScriptBlock { $Command }
+
+    $EntryContext = [PSCustomObject]@{Command = $Command;Result = $Result}
+    $Context = [PSCustomObject]@{
+        PowerShellSSH = [PSCustomObject]@{Query=$EntryContext}
+    }
+    $Contents = $Title + $Result
+
+    $DemistoResult = @{
+        Type = 1;
+        ContentsFormat = "json";
+        Contents = $Contents;
+        EntryContext = $Context;
+        ReadableContentsFormat = "markdown";
+        HumanReadable = $Contents
+    }
+    return $DemistoResult
+}
+
+function InvokeCommand ($Command)
+{
+    $Temp = $demisto.UniqueFile()
+    $FileName = $demisto.Investigation().id + "_" + $Temp
+    $Session = CreateSession
+    Copy-Item -FromSession $Session $Command -Destination $FileName
+    $Session | Remove-PSsession
+    $demisto.Results(@{
+       Type = 3;
+       ContentsFormat = "text";
+       Contents = "";
+       File = $FileName;
+       FileID = $Temp
+    })
+}
+
+$demisto.Info("Current command is: " + $demisto.GetCommand())
+
+switch -Exact ($demisto.GetCommand())
+{
+    'test-module' {
+        $TestConnection = InvokeCommand('$PSVersionTable')
+        $demisto.Results('ok'); Break
+    }
+    'pwsh-remoting-query' {
+        $Command = $demisto.Args().command
+        $RunCommand = InvokeCommand($Command)
+        $demisto.Results($RunCommand); Break
+    }
+    'pwsh-download-file' {
+        $Path = $demisto.Args().path
+        $FileResult = DownloadFile($Path)
+        $demisto.Results($FileResult); Break
+    }
+    Default {
+        $demisto.Error('Unsupported command was entered.')
+    }
+}
